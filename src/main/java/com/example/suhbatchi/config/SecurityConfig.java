55f293c239dcab59b5531bfbe87033385d1dbd2a
DiyorPulat <Xoliqulov2412@gmail.com>
2025-03-29 16:28:13 +0500

    cors removed

--- src/main/java/com/example/suhbatchi/config/SecurityConfig.java
+++ src/main/java/com/example/suhbatchi/config/SecurityConfig.java
@@ -36,14 +36,7 @@ public class SecurityConfig {
         log.info("http {}", http);
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(request -> {
-                    CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("*")); // yoki frontend domenini qo‘ying
-                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                    config.setAllowedHeaders(List.of("*"));
-                    config.setAllowCredentials(true);
-                    return config;
-                }))
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**", "/login/**", "/auth/authenticate").permitAll()
                         .anyRequest().authenticated())
